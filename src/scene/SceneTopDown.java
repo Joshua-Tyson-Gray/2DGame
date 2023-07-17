b618a8c0d38c24e0ef9924adfe2133858cf04480
Joshua Gray <54373721+Joshua-Tyson-Gray@users.noreply.github.com>
2023-07-17 00:27:21 -0600

    Removed imports
    Removed unneeded imports from the Scene class.

--- src/scene/SceneTopDown.java
+++ src/scene/SceneTopDown.java
@@ -1,10 +1,6 @@
 package scene;
 
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
